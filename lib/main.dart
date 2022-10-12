9e1517a5bbec9a11da78d5cd287ca57021536c84
Bilal Ahmad <bilalahmad8739@gmail.com>
2022-10-12 13:50:55 +0500

    some more changes

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    //Some changes
+    //Some changes done
     return MaterialApp(
         title: 'Flutter Chat UI',
         debugShowCheckedModeBanner: false,
